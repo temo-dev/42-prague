7516f90f8a5fc0db51068553a845de29a3f63ecb
TuanAnh-Seta <anhnt6879@seta-international.vn>
2024-06-12 22:42:25 +0200

    add-new-ex

--- c02/ex05/ft_str_is_uppercase.c
+++ c02/ex05/ft_str_is_uppercase.c
@@ -1,16 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_str_is_alpha.c                                  :+:      :+:    :+:   */
+/*   ft_str_is_lowercase.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tuanguye <tuanguye@student.42prague.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/12 12:42:36 by tuanguye          #+#    #+#             */
-/*   Updated: 2024/06/12 14:17:20 by tuanguye         ###   ########.fr       */
+/*   Updated: 2024/06/12 15:30:01 by tuanguye         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-// #include <stdio.h>
+#include <stdio.h>
 
 int ft_strlen(char *str)
 {
@@ -24,7 +24,7 @@ int ft_strlen(char *str)
 	return (i);
 }
 
-int ft_check_alphabet(char *str)
+int ft_check_uppercase(char *str)
 {
 	int i;
 	int is_only_alphabet;
@@ -32,9 +32,10 @@ int ft_check_alphabet(char *str)
 	i = 0;
 	while (str[i] != '\0')
 	{
+		printf("%c \n", str[i]);
 		is_only_alphabet = 0;
 		if (
-			str[i] >= 97 && str[i] <= 122 || str[i] >= 65 && str[i] <= 90 || str[i] == 32)
+			str[i] >= 65 && str[i] <= 90 || str[i] == 32)
 		{
 			is_only_alphabet = 1;
 		}
@@ -47,7 +48,7 @@ int ft_check_alphabet(char *str)
 	return (is_only_alphabet);
 }
 
-int ft_str_is_alpha(char *str)
+int ft_str_is_lowercase(char *str)
 {
 	int i;
 	int number_of_char;
@@ -61,15 +62,15 @@ int ft_str_is_alpha(char *str)
 	}
 	else
 	{
-		return (ft_check_alphabet(str));
+		return (ft_check_uppercase(str));
 	}
 }
 
-// int main(void)
-// {
-//   char str_1[20] = "ha aUaa";
-//   char str_2[20] = "!@#$%^&";
-//   char str_3[20] = "";
-//   printf("%d \n", ft_str_is_alpha(str_3));
-//   return 0;
-// }
+int main(void)
+{
+	char str_1[20] = "AAAKKKK";
+	char str_2[20] = "!@#$%^&";
+	char str_3[20] = "";
+	printf("result:%d \n", ft_str_is_lowercase(str_1));
+	return 0;
+}
--- c02/ex06/ft_str_is_printable.c
+++ c02/ex06/ft_str_is_printable.c
@@ -1,16 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_str_is_alpha.c                                  :+:      :+:    :+:   */
+/*   ft_str_is_lowercase.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tuanguye <tuanguye@student.42prague.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/12 12:42:36 by tuanguye          #+#    #+#             */
-/*   Updated: 2024/06/12 14:17:20 by tuanguye         ###   ########.fr       */
+/*   Updated: 2024/06/12 15:30:01 by tuanguye         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-// #include <stdio.h>
+#include <stdio.h>
 
 int ft_strlen(char *str)
 {
@@ -24,17 +24,16 @@ int ft_strlen(char *str)
 	return (i);
 }
 
-int ft_check_alphabet(char *str)
+int ft_check_printable(char *str)
 {
 	int i;
 	int is_only_alphabet;
-
 	i = 0;
 	while (str[i] != '\0')
 	{
 		is_only_alphabet = 0;
 		if (
-			str[i] >= 97 && str[i] <= 122 || str[i] >= 65 && str[i] <= 90 || str[i] == 32)
+			str[i] >= 32 && str[i] <= 126)
 		{
 			is_only_alphabet = 1;
 		}
@@ -47,7 +46,7 @@ int ft_check_alphabet(char *str)
 	return (is_only_alphabet);
 }
 
-int ft_str_is_alpha(char *str)
+int ft_str_is_printable(char *str)
 {
 	int i;
 	int number_of_char;
@@ -61,15 +60,15 @@ int ft_str_is_alpha(char *str)
 	}
 	else
 	{
-		return (ft_check_alphabet(str));
+		return (ft_check_printable(str));
 	}
 }
 
-// int main(void)
-// {
-//   char str_1[20] = "ha aUaa";
-//   char str_2[20] = "!@#$%^&";
-//   char str_3[20] = "";
-//   printf("%d \n", ft_str_is_alpha(str_3));
-//   return 0;
-// }
+int main(int argc, char *argv[])
+{
+	char string[40] = "Coucoutu vas bien ?";
+	int i;
+	i = ft_str_is_printable(string);
+	printf("result:%d \n", i);
+	return 0;
+}
